79241ed78c847a7b700fce548fc51f38c0af1030
Dmitry Komkov <drnecromant@gmail.com>
2018-04-22 22:20:26 +0200

    Implement getPosts in blog service

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { AuthService } from './auth.service'
 
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class BlogService {
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) { }
+
+  private getAuthHeaders(token: string): HttpHeaders {
+    // Inject JWT token to request
+    return new HttpHeaders({ Authorization: `JWT ${token}`});
+  }
+
+  getPosts(): Observable<any> {
+    return this.http.get('http://localhost:8000/api/v1/blog/', {
+      headers: this.getAuthHeaders(this.authService.getToken())
+    });
+  }
 }
