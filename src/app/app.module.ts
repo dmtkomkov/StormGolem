57276b0563691188457e1fb809a8b64b68085c6b
Dmitry Komkov <drnecromant@gmail.com>
2018-05-01 19:45:12 +0200

    Add routing for home and blog components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,21 +10,32 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginDialogComponent } from './dialogs/login-dialog/login-dialog.component';
 
 import { AuthService } from './services/auth.service';
 import { BlogService } from './services/blog.service';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginDialogComponent,
+    HomeComponent,
+    BlogComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
     HttpClientModule,
     MatIconModule,
     MatToolbarModule,
