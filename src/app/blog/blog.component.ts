453cf32f030d05e2052764261a5ba169439e60b1
Dmitry Komkov <drnecromant@gmail.com>
2018-05-01 19:44:06 +0200

    Added blog component

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sg-blog',
+  template: `
+    <p>
+      blog works!
+    </p>
+  `,
+  styles: []
+})
+export class BlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
