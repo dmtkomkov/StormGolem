57e69ee68647b5fd0c0b878e2bb4866a02a85c90
Dmitry Komkov <drnecromant@gmail.com>
2018-05-01 19:44:14 +0200

    Added home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sg-home',
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
