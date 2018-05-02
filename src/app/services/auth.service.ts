86469760c41672426c37f246b94466ca90274d53
Dmitry Komkov <drnecromant@gmail.com>
2018-05-02 16:13:24 +0200

    Added loggedIn variable in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,16 +8,26 @@ import { LoginUser, Token } from '../interfaces';
 
 @Injectable()
 export class AuthService {
+  loggedIn: boolean;
+
   constructor(
     private http: HttpClient,
-  ) { }
+  ) {
+    this.loggedIn = this.getToken()? true: false;
+  }
 
   // Send authentication request
   auth(loginUser: LoginUser): Observable<Token> {
     return this.http.post<Token>('http://localhost:8000/auth/', loginUser).do(
-        data => this.setToken(data.token),
-        error => this.setToken(null),
-      );
+      data => {
+        this.setToken(data.token);
+        this.loggedIn = true;
+      },
+      error => {
+        this.setToken(null);
+        this.loggedIn = false;
+      },
+    );
   }
 
   setToken(token: string) {
