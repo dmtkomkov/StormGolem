c5d2044435b87ecd777f17f8badd1e6651081743
Dmitry Komkov <drnecromant@gmail.com>
2018-05-02 04:01:47 +0200

    Implemented getUser in user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { AuthService } from './auth.service'
+
+import { Observable } from 'rxjs/Observable';
+
+import { User } from '../interfaces';
 
 @Injectable()
 export class UserService {
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) { }
 
-  constructor() { }
+  private getAuthHeaders(token: string): HttpHeaders {
+    // Inject JWT token to request
+    return new HttpHeaders({ Authorization: `JWT ${token}`});
+  }
 
+  getUser(): Observable<User> {
+    return this.http.get<User>('http://localhost:8000/api/v1/user/', {
+      headers: this.getAuthHeaders(this.authService.getToken())
+    });
+  }
 }
