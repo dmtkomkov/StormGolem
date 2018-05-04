fa8f114b6f46dda2a9332b766c5a001b672d3d13
Dmitry Komkov <drnecromant@gmail.com>
2018-05-04 15:02:00 +0200

    Initial blog template

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../services/blog.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'sg-blog',
-  template: `
-    <p>
-      blog works!
-    </p>
-  `,
-  styles: []
+  templateUrl: 'blog.component.html',
+  styles: [],
 })
 export class BlogComponent implements OnInit {
+  posts: any = [];
 
-  constructor() { }
+  constructor(
+    private blogService: BlogService,
+  ) { }
 
   ngOnInit() {
+    this.blogService.getPosts().subscribe(
+      blogPage => {
+        console.log(blogPage);
+        this.posts = blogPage.results;
+      },
+      (error: HttpErrorResponse) => {
+        console.log('Error Posts: ', error.message);
+        this.posts = [];
+      },
+    )
   }
-
 }
