971fece89047ef3ce663461feadbb8a4c51aa33e
Dmitry Komkov <drnecromant@gmail.com>
2018-05-20 14:17:56 +0200

    Build form group for new post

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../services/blog.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Post, BlogPage } from '../interfaces';
 
@@ -12,12 +13,18 @@ import { Post, BlogPage } from '../interfaces';
 export class BlogComponent implements OnInit {
   posts: Post[] = [];
   newDate: Date = new Date();
+  postForm: FormGroup;
 
   constructor(
     private blogService: BlogService,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit() {
+    this.postForm = this.fb.group({
+      title: ['', Validators.required ],
+      body: ['', Validators.required ],
+    });
     this.blogService.getBlogPage().subscribe(
       (blogPage: BlogPage) => {
         console.log(blogPage);
