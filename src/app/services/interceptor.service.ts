07a93c5cb976ac58b66d0f61e7c0248ed226260e
Dmitry Komkov <drnecromant@gmail.com>
2019-01-20 15:01:54 +0100

    Update token before 10 minutes expired

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -7,16 +7,24 @@ import {
   HttpHeaders,
 } from '@angular/common/http';
 
+import { TokenDto } from '@interfaces';
+
+import { AuthService } from '@services/auth.service';
+
 import { Observable } from 'rxjs';
 
 import { environment } from '@environments';
+
+import * as decode from "jwt-decode";
 
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
   baseUrl: string;
   apiUrl: string;
 
-  constructor() {
+  constructor(
+    private authService: AuthService,
+  ) {
     this.baseUrl = environment.backend;
     this.apiUrl = environment.backend + environment.api
   }
@@ -28,6 +36,17 @@ export class InterceptorService implements HttpInterceptor {
       url: this.getModifiedUrl(request.url),
     });
 
+    if (request.url !== 'auth' && request.url !== 'refresh') {
+      const token: string = sessionStorage.getItem('token');
+      const current = Math.round(+new Date()/1000);
+      if (token) {
+        const tokenDto: TokenDto = decode(token);
+        if (tokenDto.exp - current < 600) {
+          this.authService.refresh();
+        }
+      }
+    }
+
     return next.handle(modifiedRequest);
   }
 
@@ -38,7 +57,7 @@ export class InterceptorService implements HttpInterceptor {
 
   private getModifiedUrl(url: string): string {
     if (url.startsWith('assets')) return url // Do not modify assets url
-    else if (url === 'auth') return this.baseUrl + url; // Use base url for auth
+    else if (url === 'auth' || url === 'refresh') return this.baseUrl + url; // Use base url for auth
     else return this.apiUrl + url; // Use api url for all other requests
   }
 }
