a5bb7507391dd923e851690f66ae01c9724dd1ef
Dmitry Komkov <drnecromant@gmail.com>
2019-04-10 21:05:48 +0200

    Get rid of select subscription #6

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BlogService } from '@services/blog.service';
 import { AuthService } from '@services/auth.service';
 
-import { BehaviorSubject, Observable, Subscription, merge, of } from 'rxjs';
-import {catchError, map, switchMap, take} from "rxjs/operators";
+import { Observable, merge, Subject} from 'rxjs';
 
-import { IBlogPost, IBlogPage } from '@interfaces';
+import { IBlogPost } from '@interfaces';
 import { Store } from '@ngrx/store';
-import { IAppState } from "../state/app.state";
-import {EBlogAction} from "../actions/blog.actions";
-import {tap} from "rxjs/internal/operators/tap";
+import { IAppState } from "../states/app.state";
+import { EBlogAction } from "../actions/blog.actions";
+import { select } from "@ngrx/store";
+import { takeUntil, tap } from "rxjs/operators";
 
 @Component({
   selector: 'sg-blog',
@@ -18,43 +18,39 @@ import {tap} from "rxjs/internal/operators/tap";
   styleUrls: ['blog.component.scss'],
 })
 export class BlogComponent implements OnInit, OnDestroy {
-  blogState: Subscription;
-  blogPageNumber$: BehaviorSubject<number>;
-  blogPageContent$: Observable<IBlogPost[]>;
-  selectedBlogPost: number;
-  emptyBlogPost: IBlogPost;
-  pageRefresh: Subscription;
+  public blogPageContent$: Observable<IBlogPost[]>;
+  public selectedBlogPost: number;
+  public emptyBlogPost: IBlogPost;
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
     private authService: AuthService,
     private blogService: BlogService,
     private store: Store<IAppState>,
   ) {
-    this.blogPageNumber$ = new BehaviorSubject(0);
     this.emptyBlogPost = {id: 0, title: '', body: ''};
   }
 
   ngOnInit() {
-    this.blogState = this.store.select(state => state).pipe(
-      tap(result => console.log('select result:', result))
-    ).subscribe();
-    this.blogPageContent$ = this.blogPageNumber$.asObservable().pipe(
-      switchMap(() => this.blogService.getBlogPage().pipe(
-        catchError(() => of({results: null})), // Return page with null results on error
-        take(1),
-      )),
-      map((blogPage: IBlogPage): IBlogPost[] => blogPage.results),
+    this.blogPageContent$ = this.store.pipe(
+      takeUntil(this.ngUnsubscribe),
+      select((state: IAppState) => state.blog.blogPosts),
+      tap(result => console.log(result)),
     );
-    this.pageRefresh = merge(this.authService.loggedIn$, this.blogService.action$).subscribe(() => {
+
+    this.store.dispatch({ type: EBlogAction.GetBlogPosts });
+
+    merge(this.authService.loggedIn$, this.blogService.action$).pipe(
+      takeUntil(this.ngUnsubscribe),
+    ).subscribe(() => {
       this.store.dispatch({ type: EBlogAction.GetBlogPosts });
       this.selectBlogPost(NaN);
-      this.blogPageNumber$.next(0);
     });
   }
 
   ngOnDestroy(): void {
-    this.pageRefresh.unsubscribe();
-    this.blogState.unsubscribe();
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   selectBlogPost(blogPostId: number) {
