32d0f014a17c928edc712fe55ffe85c7197b856c
Dmitry Komkov <drnecromant@gmail.com>
2019-04-21 23:42:52 +0200

    Add some shortcuts and TODOs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ngrxLogger } from "./shared/helpers/logger.helpers";
+import { ngrxLogger } from "@shared/helpers/logger.helpers";
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,30 +14,32 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 
-import { AppComponent } from './root/app.component'
-import { HomeComponent } from './root/home/home.component';
-import { BlogComponent } from './root/blog/blog.component';
-import { PostComponent } from './root/blog/post/post.component';
-import { PostFormComponent } from './root/blog/post/post-form/post-form.component';
-import { LoginDialogComponent } from './shared/dialogs/login-dialog/login-dialog.component';
-import { PageNotFoundComponent } from './shared/errors/page-not-found/page-not-found.component';
+import { AppComponent } from '@root/app.component'
+import { HomeComponent } from '@root/home/home.component';
+import { BlogComponent } from '@root/blog/blog.component';
+import { PostComponent } from '@root/blog/post/post.component';
+import { PostFormComponent } from '@root/blog/post/post-form/post-form.component';
+import { LoginDialogComponent } from '@shared/dialogs/login-dialog/login-dialog.component';
+import { PageNotFoundComponent } from '@shared/errors/page-not-found/page-not-found.component';
 
-import { AuthService } from './shared/services/auth.service';
-import { UserService } from './shared/services/user.service';
-import { BlogService } from './shared/services/blog.service';
-import { GuardService } from './shared/services/guard.service';
-import { InterceptorService } from './shared/services/interceptor.service';
+import { AuthService } from '@shared/services/auth.service';
+import { UserService } from '@shared/services/user.service';
+import { BlogService } from '@shared/services/blog.service';
+import { GuardService } from '@shared/services/guard.service';
+import { InterceptorService } from '@shared/services/interceptor.service';
 
 import { StoreModule } from "@ngrx/store";
-import { Routes, RouterModule } from '@angular/router';
-import { blogReducer } from "./store/reducers/blog.reducer";
 import { EffectsModule } from "@ngrx/effects";
-import { BlogEffect } from "./store/effects/blog.effect";
-import { authReducer } from "./store/reducers/auth.reducer";
-import { AuthEffect } from "./store/effects/auth.effect";
-import { userReducer } from "./store/reducers/user.reducer";
-import { UserEffect } from "./store/effects/user.effect";
+import { Routes, RouterModule } from '@angular/router';
 
+import { blogReducer } from "@store/reducers/blog.reducer";
+import { BlogEffect } from "@store/effects/blog.effect";
+import { authReducer } from "@store/reducers/auth.reducer";
+import { AuthEffect } from "@store/effects/auth.effect";
+import { userReducer } from "@store/reducers/user.reducer";
+import { UserEffect } from "@store/effects/user.effect";
+
+// TODO: routers, store and effects into separate modules
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'blog', component: BlogComponent, canActivate: [ GuardService ] },
--- src/app/store/states/app.state.ts
+++ src/app/store/states/app.state.ts
@@ -2,6 +2,7 @@ import { IBlogState } from "./blog.state";
 import { IAuthState } from "./auth.state";
 import { IUserState } from "./user.state";
 
+// TODO: import from index.ts
 export interface IAppState {
   blog: IBlogState;
   auth: IAuthState;
