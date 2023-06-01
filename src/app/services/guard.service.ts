6eee389f4b1d844d5a1f1c923423ce9d35d8b2eb
Dmitry Komkov <dmtkomkov@gmail.com>
2023-06-01 03:26:40 +0200

    Angular update

--- src/app/services/guard.service.ts
+++ src/app/services/guard.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+
 
 @Injectable({providedIn: 'root'})
-export class GuardService implements CanActivate {
+export class GuardService  {
   canActivate() {
     return true;
   }
